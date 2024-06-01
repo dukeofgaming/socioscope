ff859c62202c9f6cfa86f2c9fedd4c62b89d8812
David Vega <dukeofgaming@gmail.com>
2024-06-01 16:01:23 -0700

    Passing unit test

--- socioscope/__init__.py
+++
@@ -1,8 +0,0 @@
-
-import subprocess
-
-def main():
-    subprocess.run(['echo', 'Hello World'], check=True)
-
-if __name__ == "__main__":
-    main()
--- socioscope/socioscope.py
+++ socioscope/socioscope.py
@@ -1,7 +1,7 @@
 import subprocess
 
 def main():
-    subprocess.run(['echo', 'Hello World'], check=True)
+    subprocess.run(['echo', 'Hello World!'], check=True)
 
 if __name__ == "__main__":
     main()
