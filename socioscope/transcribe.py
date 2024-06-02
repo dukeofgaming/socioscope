ede9ed1f0a0742ab78e803845bb250bf603a3854
David Vega <dukeofgaming@gmail.com>
2024-06-02 11:52:34 -0700

    Adding supported file formats, fixing test (exception was being caught)

--- socioscope/transcribe.py
+++ socioscope/transcribe.py
@@ -1,8 +1,8 @@
-
+audio_file_types = (".mp3", ".wav", ".flac", ".aac", ".m4a", ".ogg")
 
 def transcribe_audio(file_path):
 
-    if not file_path.lower().endswith('.m4a'):
-        raise ValueError("The file must be an m4a audio file.")
+    if not file_path.lower().endswith(audio_file_types):
+        raise ValueError("The file must be an audio file.")
     
     print(f"Transcribing {file_path}...")
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -6,27 +6,34 @@ from unittest.mock import patch
 
 from socioscope import cli
 
+#Arrange
 @patch.object(sys, 'argv', new=[cli.__package__, '-h'])
 def test_that_cli_help_message_displays_correctly(capfd):
-    
+
+    #Act
     with pytest.raises(SystemExit):
         cli.main()
     
     out, err = capfd.readouterr()
+
+    #Assert
     assert f"usage: {cli.__package__} [-h]" in out
     assert "show this help message and exit" in out
 
-@patch.object(sys, 'argv', new=['cli', 'transcribe', 'audio.m4a'])
+#Arrange
+@patch.object(sys, 'argv', new=[cli.__package__, 'transcribe', 'audio.m4a'])
 def test_transcribe_m4a_file(capfd):
     cli.main()
     out, err = capfd.readouterr()
     assert "Transcribing audio.m4a..." in out
 
-@patch.object(sys, 'argv', new=['cli', 'transcribe', 'audio.mp3'])
-def test_transcribe_fail_on_non_m4a_file(capfd):
-    try:
-        cli.main()
-    except ValueError as exception:
-        out, err = capfd.readouterr()
-        assert "The file must be an m4a audio file." in err
+#Arrange
+@patch.object(sys, 'argv', new=[cli.__package__, 'transcribe', 'audio.txt'])
+def test_error_transcribe_audio_format(capfd):
+    #Act
+    cli.main()
+    out, err = capfd.readouterr()
 
+    #Assert
+    assert "The file must be an audio file." in err
+    
