eb07ce87c31d8a34e7fbaa19e19abe7e5c201e77
David Vega <dukeofgaming@gmail.com>
2024-06-01 17:51:26 -0700

    Adding help text for default command invocation

--- socioscope/cli.py
+++ socioscope/cli.py
@@ -1,7 +1,38 @@
+import argparse
 import subprocess
+import sys
 
 def main():
-    subprocess.run(['echo', 'Hello World!'], check=True)
+    parser = argparse.ArgumentParser(
+        prog        = __package__,
+        description = "socioscope CLI tool"
+    )
 
-if __name__ == "__main__":
-    main()
+    subparsers = parser.add_subparsers(
+        dest        = 'command', 
+        required    = False
+    )
+
+    # transcribe_parser = subparsers.add_parser(
+    #     'transcribe', 
+    #     help='Transcribe an m4a audio file'
+    # )
+
+    # transcribe_parser.add_argument(
+    #     'file', 
+    #     type=str, 
+    #     help='The path to the m4a file to transcribe', 
+    #     metavar='FILE'
+    # )
+
+    args = parser.parse_args()
+
+    if args.command == 'transcribe':
+        # file_path = args.file
+        # if not file_path.lower().endswith('.m4a'):
+        #     print("Error: The file must be an m4a audio file.")
+        #     sys.exit(1)
+        print(f"Transcribing {args.file}...")
+    else:
+        parser.print_help()
+        
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -3,7 +3,14 @@ from unittest.mock import patch
 
 from socioscope import cli
 
-@patch('subprocess.run')
-def test_should_return_hello_world_bang(mock_run):
-    cli.main()
-    mock_run.assert_called_once_with(['echo', 'Hello World!'], check=True)
+def test_that_cli_help_message_displays_correctly(capfd):
+    with patch('sys.argv', ['cli.__package__', '-h']), pytest.raises(SystemExit):
+        cli.main()
+    out, err = capfd.readouterr()
+    assert f"usage: {cli.__package__} [-h]" in out
+    assert "show this help message and exit" in out
+
+# @patch('sys.argv', ['cli', 'transcribe', 'audio.m4a'])
+# def test_transcribe_command(mock_run):
+#     cli.main()
+#     mock_run.assert_called_once_with(['echo', 'Transcribing audio.m4a...'], check=True)
