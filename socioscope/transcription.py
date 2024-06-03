199668a7ec229ef0cdc6fc617e05a60a49e8f711
David Vega <dukeofgaming@gmail.com>
2024-06-03 00:33:10 -0700

    Adding wav conversion with resampling to 16khz

--- socioscope/transcription.py
+++ socioscope/transcription.py
@@ -1,27 +1,53 @@
+import os
 import subprocess
 from socioscope import messages
 
-supported_audio_formats = (".wav")
+supported_audio_formats = (".wav", ".m4a")
 
 def transcribe_audio(file_path):
 
-    if not file_path.lower().endswith(supported_audio_formats):
+    #Get file extension from path
+    file_extension = os.path.splitext(file_path)[1]
+
+    if not file_extension in supported_audio_formats:
         raise ValueError(
             messages.INVALID_AUDIO_TRANSCRIPTION_FORMAT.format(
                 file_extensions=', '.join(supported_audio_formats)
             )
         )
     
+    if file_extension != ".wav":
+        convert_to_wav(file_path)
+        file_path = os.path.splitext(file_path)[0] + ".wav" # Update file path to the converted file
+    
     print(f"Transcribing {file_path}...")
 
     run_whisper(file_path)
 
+
 def run_whisper(file_path):
-    command = [
+    subprocess.run([
         'whisper-cpp',          # Path to the compiled whisper-cpp executable
-        '--model', '/Users/david/Projects/socioscope/tests/.models/ggml-large-v3.bin',     # Model file path
+        # '--model', '/Users/david/Projects/socioscope/tests/.models/ggml-large-v3.bin',     # Model file path
+        '--model', '/Users/david/Projects/socioscope/tests/.models/ggml-base.en.bin',     # Model file path
         '--file', file_path,     # Input audio file path
         '--output-csv'          # Output file path (if needed)
-    ]
+    ], check=True)
 
-    subprocess.run(command, check=True)
+
+def convert_to_wav(file_path):
+    base                = os.path.basename(file_path)   #TODO: See if this is necessary
+    directory           = os.path.dirname(file_path)
+
+    name_without_ext    = os.path.splitext(base)[0]
+    output_file_path    = os.path.join(directory, f"{name_without_ext}.wav")
+
+    print(f"Converting {file_path} to {output_file_path}...")
+    
+    subprocess.run([
+        'ffmpeg', 
+        '-i', file_path, 
+        '-ar', '16000',     # Audio rate supported by whisper-cpp
+        '-y',               #TODO: Optimize # Overwrite output file if it exists
+        output_file_path
+    ], check=True)
--- tests/test_transcription.py
+++ tests/test_transcription.py
@@ -8,10 +8,11 @@ from socioscope import cli
 @patch.object(sys, 'argv', new=[cli.__package__, 'transcribe', './tests/data/jfk.wav'])
 def test_functional_transcribe_wav_file(capfd):
     #Arrange
-    wav_file_path = './tests/data/jfk.wav'
-    txt_file_path = f"{wav_file_path}.csv"
-    if os.path.exists(txt_file_path):
-        os.remove(txt_file_path)
+    wav_file_path           = './tests/data/jfk.wav'
+    transcription_file_path = f"{wav_file_path}.csv"
+
+    if os.path.exists(transcription_file_path):
+        os.remove(transcription_file_path)
     
     assert os.path.exists(wav_file_path)
     
@@ -19,4 +20,25 @@ def test_functional_transcribe_wav_file(capfd):
     cli.main()
 
     #Assert
-    assert os.path.exists(txt_file_path)
+    assert os.path.exists(transcription_file_path)
+
+
+@patch.object(sys, 'argv', new=[cli.__package__, 'transcribe', './tests/data/recordings/N Ninth St 3.m4a'])
+def test_functional_transcribe_m4a_file(capfd):
+    #Arrange
+    m4a_file_path           = './tests/data/recordings/N Ninth St 3.m4a'
+    wav_file_path           = f"{os.path.splitext(m4a_file_path)[0]}.wav"
+    transcription_csv_path  = f"{wav_file_path}.csv"
+
+    if os.path.exists(wav_file_path):
+        os.remove(wav_file_path)
+
+    if os.path.exists(transcription_csv_path):
+        os.remove(transcription_csv_path)
+
+    #Act
+    cli.main()
+    assert os.path.exists(wav_file_path)
+
+    #Assert
+    assert os.path.exists(transcription_csv_path)
